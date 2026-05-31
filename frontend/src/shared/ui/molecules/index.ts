997f69e692a4335b2da05821f23cf9391a2a974b
Joiver González <gjoiver@gmail.com>
2026-05-31 15:31:31 -0500

    feat(products): implement pagination

--- frontend/src/shared/ui/molecules/index.ts
+++ frontend/src/shared/ui/molecules/index.ts
@@ -2,3 +2,5 @@ export * from "./Card";
 export * from "./Expandable";
 export * from "./SectionTitle";
 export * from "./ErrorState";
+export * from "./RichTextRenderer";
+export * from "./Pagination";
